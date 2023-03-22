8662b9472043b2f3b81f0f111e45fa73603720f6
earn <phanwaphat.e@mail.kmutt.ac.th>
2023-03-22 19:31:48 +0700

    name

--- lab3 motor/Core/Src/main.c
+++ lab3 motor/Core/Src/main.c
@@ -22,6 +22,7 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #define IC_BUFFER_SIZE 20
+//Phanwaphat Lohasummakul 69
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
